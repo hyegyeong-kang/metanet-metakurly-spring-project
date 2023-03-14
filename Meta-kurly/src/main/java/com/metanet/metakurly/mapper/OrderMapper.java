93863f431fee6cab50ac0ce601c79b61b487bb1f
lhyeonj120 <isk04387@gmail.com>
2023-03-14 18:32:25 +0900

    chore: update files

--- Meta-kurly/src/main/java/com/metanet/metakurly/mapper/OrderMapper.java
+++ Meta-kurly/src/main/java/com/metanet/metakurly/mapper/OrderMapper.java
@@ -2,12 +2,14 @@ package com.metanet.metakurly.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.metanet.metakurly.domain.OrderDTO;
 import com.metanet.metakurly.domain.OrderProductDTO;
 
 public interface OrderMapper {
 	
-	public OrderDTO getOrder(Long m_id, Long o_id);
+	public OrderDTO getOrder(Long o_id);
 	
 	public List<OrderDTO> getOrderList(Long m_id);
 
@@ -17,7 +19,7 @@ public interface OrderMapper {
 	
 	public void createOrderDetail(OrderDTO order);
 	public void createOrder(OrderDTO order);
-	public void modifyOrder(OrderDTO order);
+	//public void modifyOrder(OrderDTO order);
 	//public void createOrder(OrderProductDTO orderProduct);
 	
 	public int cancelOrder(Long o_id);
